9c99386454eb915f7861772ae9b876cbb0fbb1fa
vadim <wesborlandzp@gmail.com>
2017-12-04 23:10:19 +0200

    sortByDaysFunc

--- src/api.js
+++ src/api.js
@@ -34,32 +34,60 @@ export default class FetchData extends React.PureComponent {
 
         const getData = Future.encaseP(axios.get);
 
-        const list = getData('http://api.openweathermap.org/data/2.5/forecast?id=687700&units=metric&APPID=589954fc426476988cc0be8d6ed03349').fork(
-            (error) => console.log('Something went wrong, the future was rejected', error),
-            (success) => console.log(success.data.list.map(obj => {
-                return Object.assign(obj, {
-                    data:obj.dt_txt.split(' ')[0],
-                    time:obj.dt_txt.split(' ')[1]
-                   } )
-                   
-            }))
-        );
-
-
+        const usizedList = getData('http://api.openweathermap.org/data/2.5/forecast?id=687700&units=metric&APPID=589954fc426476988cc0be8d6ed03349')
+            .map(res => res.data.list.map(obj => (
+                 R.merge(obj, {
+                    data: obj.dt_txt.split(' ')[0],
+                    time: obj.dt_txt.split(' ')[1]
+                })
+            )))
 
             
+        const wrapper = () => {
+
+            let sortedData = [];
+
+            
+
+            return function sortByDaysFunc(res) {
+
+                const getDay = R.compose(R.propEq('data'), R.prop('data'), R.head);
+
+                const getRelativeData = R.filter(getDay(res));
+
+                sortedData.push(getRelativeData(res))
+
+                // console.log(getRelativeData(res));
+
+                const relativeDataLen = R.length(getRelativeData(res));
+
+                const removeFromRes = R.compose(R.drop(relativeDataLen));
+
+                const resWithoutCurrentDay = removeFromRes(res);
+
+                console.log(sortedData)
+            
+                // sortByDaysFunc(resWithoutCurrentDay)
+               
+            }
+
+
+        }
+
+    /*     const sortByDaysFunc = (res) => {
+
+
+        }  */ 
+
+        usizedList.fork(console.error, wrapper());
 
         axios.get('http://api.openweathermap.org/data/2.5/forecast?id=687700&units=metric&APPID=589954fc426476988cc0be8d6ed03349')
             .then(res => {
 
-               
-
                 const data = res.data.list;
                 const sortedData = [{}];
 
-                const sameDay = R.propEq('dt_txt', '2017-12-03 21:00:00');
                 // console.log(R.filter(sameDay, data));
-
 
                 let j = 0;
                 let k = 0;
