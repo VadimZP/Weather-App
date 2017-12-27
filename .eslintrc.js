bfe40102401a7b611165fdba6fe3ee63f4a157fc
vadim <wesborlandzp@gmail.com>
2017-12-27 15:28:02 +0200

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "extends": [
+        "airbnb",
+    ],
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "indent": ["error", 4],
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
+        "react/jsx-filename-extension": 0,
+        "react/forbid-prop-types": 0,
+        "react/require-default-props": 0,
+        "no-undef": 0,
+        "no-underscore-dangle": 0,
+        "exceptMethods": [
+            "componentDidMount",
+            "componentDidUpdate",
+            "componentWillMount",
+            "componentWillReceiveProps",
+            "componentWillUnmount",
+            "componentWillUpdate",
+            "render",
+            "shouldComponentUpdate",
+        ]
+    }
+};
