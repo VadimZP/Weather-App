cd3987103c576b97812eea311ade8c98df043438
vadim <wesborlandzp@gmail.com>
2017-12-29 02:56:15 +0200

    some rules were added

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,13 +9,14 @@ module.exports = {
     ],
     "rules": {
         "indent": ["error", 4],
+        "semi": ["error", "never"],
+        "no-undef": 0,
+        "no-underscore-dangle": 0,
         "react/jsx-indent": ["error", 4],
         "react/jsx-indent-props": ["error", 4],
         "react/jsx-filename-extension": 0,
         "react/forbid-prop-types": 0,
         "react/require-default-props": 0,
-        "no-undef": 0,
-        "no-underscore-dangle": 0,
         "exceptMethods": [
             "componentDidMount",
             "componentDidUpdate",
