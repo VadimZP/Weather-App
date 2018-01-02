d1920ad9bfb2c4cad38ce598327be07d8ac98501
vadim <wesborlandzp@gmail.com>
2018-01-02 16:59:33 +0200

    vx was replaced by chart.js

--- src/components/Graph/GraphContainer.js
+++ src/components/Graph/GraphContainer.js
@@ -1,102 +1,96 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { GlyphDot } from '@vx/glyph'
-import { scaleTime, scaleLinear } from '@vx/scale'
-import { extent, max, min } from 'd3-array'
-
 import GraphView from './GraphView'
 
-GraphContainer.propTypes = {
-    day: PropTypes.arrayOf(PropTypes.object),
+
+export default class GraphContainer extends React.Component {
+   static propTypes = {
+       displayLegend: true,
+       day: PropTypes.arrayOf(PropTypes.object),
+   }
+
+   state = {
+       chartData: {
+           labels: null,
+           datasets: [
+               {
+                   data: null,
+                   fill: 'start',
+                   backgroundColor: null,
+               },
+           ],
+       },
+       chartOptions: {
+           scales: {
+               yAxes: [{
+                   gridLines: {
+                       display: false,
+                   },
+                   ticks: {
+                       beginAtZero: true,
+                   },
+               }],
+               xAxes: [{
+                   gridLines: {
+                       display: false,
+                   },
+               }],
+           },
+           legend: {
+               display: this.props.displayLegend,
+           },
+           tooltips: {
+               intersect: false,
+           },
+       },
+   }
+
+
+   componentWillReceiveProps(nextProps) {
+       if (this.props.day !== nextProps.day) {
+           const { chartData } = this.state
+
+           const time = nextProps.day.map(obj => obj.time)
+           const temp = nextProps.day.map(obj => obj.main.temp)
+
+           const { ctx } = this.el.chart_instance
+
+           const gradientStroke = ctx.createLinearGradient(500, 0, 100, 0)
+           gradientStroke.addColorStop(0, '#80b6f4')
+           gradientStroke.addColorStop(1, '#f49080')
+
+           const gradientFill = ctx.createLinearGradient(500, 0, 100, 0)
+           gradientFill.addColorStop(0, 'rgba(128, 182, 244, 0.6)')
+           gradientFill.addColorStop(1, 'rgba(244, 144, 128, 0.6)')
+
+           const obj = {
+               data: temp,
+               fill: 'start',
+               backgroundColor: gradientFill,
+           }
+
+           this.setState({ chartData: { ...chartData, labels: time, datasets: [obj] } })
+
+           return true
+       }
+
+       return false
+   }
+
+
+   getElem = (el) => {
+       this.el = el
+   }
+
+   render() {
+       return (
+           <GraphView
+               chartOptions={this.state.chartOptions}
+               chartData={this.state.chartData}
+               getElem={this.getElem}
+           />
+       )
+   }
 }
 
-export default function GraphContainer({ day }) {
-    /** Don't render if data wasn't fetched */
-
-    if (day === undefined) return null
-
-    /**
-     * Extract temperature and time properties from day array objects,
-     * then put these data in object.
-     * @param {array} dayArr - array of day's weather for each 3 hour.
-     * */
-
-    const getHoursAndTemperature = (dayArr) => {
-        const createObj = (obj) => {
-            const hours = +obj.time.slice(0, 2)
-
-            return { time: hours, temp: obj.main.temp }
-        }
-
-        return dayArr.map(createObj)
-    }
-
-    const data = getHoursAndTemperature(day)
-
-    /** Graph config  */
-
-    const x = d => d.time
-    const y = d => d.temp
-
-    const width = 1200
-    const height = 400
-    const margin = {
-        top: 60,
-        bottom: 60,
-        left: 80,
-        right: 80,
-    }
-
-    const xMax = width - 30
-    const yMax = height - margin.top - margin.bottom
-
-    const xScale = scaleTime({
-        range: [0, xMax],
-        domain: extent(data, x),
-    })
-
-    const yScale = scaleLinear({
-        range: [yMax, 0],
-        domain: [min(data, y), max(data, y)],
-        nice: true,
-    })
-
-    const glyphDot = (d, i) => (
-        <g key={`line-point-${i}`}>
-            <GlyphDot>
-                <text
-                    x={xScale(x(d))}
-                    y={yScale(y(d))}
-                    dx={10}
-                    fill="white"
-                    stroke="black"
-                    strokeWidth={1}
-                    fontSize={11}
-                    style={{ display: 'block', backgroundColor: 'red' }}
-                >
-                    {`${data[i].temp} °C`}
-                </text>
-            </GlyphDot>
-            <GlyphDot
-                cx={xScale(x(d))}
-                cy={yScale(y(d))}
-                r={4}
-                fill="red"
-            />
-        </g>)
-
-    return (
-        <GraphView
-            data={data}
-            width={width}
-            height={height}
-            margin={margin}
-            x={x}
-            y={y}
-            yScale={yScale}
-            xScale={xScale}
-            glyphDot={glyphDot}
-        />
-    )
-}
