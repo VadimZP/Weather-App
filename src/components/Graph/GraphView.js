183c9b7dbba0bed3a65c4973c7efcf1f4205e768
vadim <wesborlandzp@gmail.com>
2018-01-02 16:59:56 +0200

    vx was replaced by chart.js

--- src/components/Graph/GraphView.js
+++ src/components/Graph/GraphView.js
@@ -1,49 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { Group } from '@vx/group'
-import { LinePath } from '@vx/shape'
-import { curveMonotoneX } from '@vx/curve'
-import { LinearGradient } from '@vx/gradient'
+import { Line } from 'react-chartjs-2'
 
 GraphView.propTypes = {
-    data: PropTypes.arrayOf(PropTypes.object),
-    width: PropTypes.number,
-    height: PropTypes.number,
-    margin: PropTypes.object,
-    xScale: PropTypes.func,
-    yScale: PropTypes.func,
-    x: PropTypes.func,
-    y: PropTypes.func,
-    glyphDot: PropTypes.func,
+    chartData: PropTypes.object,
+    chartOptions: PropTypes.object,
+    getElem: PropTypes.func,
 }
 
-export default function GraphView(props) {
-    return (
-        <svg style={{ borderBottom: '1px solid #000' }} width={props.width} height={props.height}>
-            <LinearGradient id="gradient" to="#EFEEFC" from="#CBC8EF" />
-            <rect
-                x={0}
-                y={0}
-                width={props.width}
-                height={props.height}
-                stroke="#f1f1f1"
-                fill="url(#gradient)"
-            />
-            <Group top={props.margin.top}>
+export default function GraphView({ chartData, chartOptions, getElem }) {
+    if (chartData === undefined) return ''
 
-                <LinePath
-                    data={props.data}
-                    xScale={props.xScale}
-                    yScale={props.yScale}
-                    x={props.x}
-                    y={props.y}
-                    stroke="#7e20dc"
-                    strokeWidth={2}
-                    curve={curveMonotoneX}
-                    glyph={(d, i) => (props.glyphDot(d, i))}
-                />
-            </Group>
-        </svg>
+    return (
+        <div className="weather-graph">
+            <Line
+                data={chartData}
+                options={chartOptions}
+                ref={el => getElem(el)}
+            />
+        </div>
     )
 }
+
