a54f0b42d1c99e6411afa197e6391e2a319375a8
vadim <wesborlandzp@gmail.com>
2018-01-05 16:50:40 +0200

    styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 
-import './stylesheets/index.scss'
+import './stylesheets/main.scss'
 
 import ApiDataContainer from './components/services/ApiDataContainer'
 import SearchFormView from './components/SearchForm/SearchFormView'
